04fdc6df6f9524aa6c16b36164a9d494e2149078
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-10-09 11:29:29 -0300

    implement banking actions and page updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,19 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/banks.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
+
+  if(!accounts) return;
+
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
 
   return (
     <section className="home">
@@ -13,14 +22,14 @@ const Home = async () => {
           <HeaderBox 
             type = "greeting"
             title = "Bem vindo"
-            user={loggedIn?.name || "Guest"}
+            user={loggedIn?.firstName || "Guest"}
             subtext = "Acesse e gerencie  suas contas e transações com eficiência."
           />
 
           <TotalBalanceBox 
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.14}
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
         
@@ -29,8 +38,8 @@ const Home = async () => {
 
       <RightSidebar 
         user={loggedIn}
-        transactions={[]}
-        banks={[{ currentBalance: 114.50}, { currentBalance: 600.50}]}
+        transactions={accounts?.transactions}
+        banks={accountsData?.slice(0, 2)}
       />
     </section>
   )
--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
@@ -6,28 +6,34 @@ import { Doughnut } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
-    const data = {
-      datasets: [
-        {
-          label: "Bancos",
-          data: [1250, 2500, 3750],
-          backgroundColor: ["#fa660a", "#e65a09", "#ff9745"],
-        }
-      ],
-      labels: ["Banco 1", "Banco 2", "Banco 3"],
-    };
+  
+  const accountNames = accounts.map((a) => a.name);
+  const balances = accounts.map((a) => a.currentBalance)
 
-   return <Doughnut 
-    data={data}
-    options={{
-      cutout: "60%",
-      plugins: {
-        legend: {
-          display: false
-        }
-      }
-    }}
-   />
-}
+  const data = {
+    datasets: [
+      {
+        label: "Bancos",
+        data: balances,
+        backgroundColor: ["#fa660a", "#e65a09", "#ff9745"],
+      },
+    ],
+    labels: accountNames,
+  };
 
-export default DoughnutChart
+  return (
+    <Doughnut
+      data={data}
+      options={{
+        cutout: "60%",
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      }}
+    />
+  );
+};
+
+export default DoughnutChart;
