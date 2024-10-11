f1db9ab37069605ecb9c359884976bebcbea80e1
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-10-11 12:57:25 -0300

    implement style recent transactions and add another bank

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -4,6 +4,7 @@ import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-
 
 import { useRouter } from 'next/navigation'
 import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions'
+import Image from 'next/image'
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   const router = useRouter()
@@ -47,12 +48,28 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
           Conectar banco
         </Button>
       ): variant === "ghost" ? (
-        <Button>
-          Conectar banco
+        <Button onClick={() => open()} variant="ghost" className="plaidlink-ghost">
+          <Image 
+            src={"/icons/connect-bank.svg"}
+            alt='connect bank icon'
+            width={24}
+            height={24}
+          />
+          <p className="hidden text-[16px] font-semibold text-black-2 xl:block">
+            Conectar banco
+          </p>
         </Button>
       ): (
-        <Button>
-          Conectar banco
+        <Button onClick={() => open()} className="plaidlink-default">
+          <Image 
+            src={"/icons/connect-bank.svg"}
+            alt='connect bank icon'
+            width={24}
+            height={24}
+          />
+          <p className="text-[16px] font-semibold text-black-2">
+            Conectar banco
+          </p>
         </Button>
       )}
     </>
